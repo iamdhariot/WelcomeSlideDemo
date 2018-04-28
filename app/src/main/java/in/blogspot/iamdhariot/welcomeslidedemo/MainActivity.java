e8cd7e86e47b6ee82891d0ab8d95f815bbde92e3
iamdhariot <dharm12997@gmail.com>
2018-04-28 14:45:40 +0530

    Initial commit

---
+++ app/src/main/java/in/blogspot/iamdhariot/welcomeslidedemo/MainActivity.java
@@ -0,0 +1,29 @@
+package in.blogspot.iamdhariot.welcomeslidedemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnRestartSlider ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnRestartSlider = (Button)findViewById(R.id.btnRestartSlider);
+        btnRestartSlider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(MainActivity.this,WelcomeActivity.class));
+
+            }
+        });
+    }
+}
